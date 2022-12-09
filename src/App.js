e5324bf096460fa6db5b00a88b5347441496c448
betulerkoc <bet.er@hotmail.com>
2022-12-09 21:40:33 +0300

    implement tailwind and add sample video

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 
 function App() {
   return (
     <Container>
-      <h3>Welcome to the Textile 5.0</h3>
+      <p className="text-xl font-bold my-5">Welcome to the Textile 5.0</p>
+      <div className="my-5 mx-1">
+        <iframe
+          className="w-11/12"
+          src="https://www.youtube.com/embed/zhfAsnONwdE"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </Container>
   );
 }
