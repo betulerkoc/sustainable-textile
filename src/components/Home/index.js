a7e8f643a942925fc6878b0b558672c23e3098ae
betulerkoc <bet.er@hotmail.com>
2022-12-17 21:22:49 +0300

    add event

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,15 @@
 import Cotton from "../../Cotton.png";
 import Cloth from "../../Cloth.png";
+import { Button } from "@material-tailwind/react";
+import ReactGA from "react-ga";
+
+const GAevent = (categoryName, eventName) => {
+  ReactGA.event({       
+      category: categoryName,
+      action: eventName, 
+      label: 'labelName',        
+  });   
+}
 
 export default function Home() {
   return (
@@ -13,12 +23,21 @@ export default function Home() {
           That’s enough drinking water for one person for 900 days{" "}
         </p>
       </div>
-      <div className="w-screen h-96 mt-10 flex flex-col lg:flex-row  items-center justify-around">
-      <img className="w-20 h-20 xl:w-36 xl:h-36" src={Cloth} alt="cloth" />
-      <div className="text-xl md:text-2xl xl:text-2xl text-center"> 
-        <p className="p-2">Global textiles production almost doubled between 2000 and 2015</p>
-        <p className="p-2">About 5.8 million tonnes of textiles are discarded every year in the EU, approximately 11kg per person</p>
-        <p className="p-2">Every second somewhere in the world a truckload of textiles is landfilled or incinerated</p>
+      <div className="w-screen h-96 mt-10 flex flex-col lg:flex-row items-center justify-around">
+        <img className="w-20 h-20 xl:w-36 xl:h-36 mt-10" src={Cloth} alt="cloth" />
+        <div className="text-xl md:text-2xl xl:text-2xl text-center">
+          <p className="p-2">
+            Global textiles production almost doubled between 2000 and 2015
+          </p>
+          <p className="p-2">
+            About 5.8 million tonnes of textiles are discarded every year in the
+            EU, approximately 11kg per person
+          </p>
+          <p className="p-2">
+            Every second somewhere in the world a truckload of textiles is
+            landfilled or incinerated
+          </p>
+          <Button className="my-10 bg-black text-base" onClick={() => GAevent('Home','See Detail')}>Detail</Button>
         </div>
       </div>
       <div className="flex overflow-x-auto no-scrollbar py-10">
@@ -26,7 +45,7 @@ export default function Home() {
           className="w-full h-56 mr-5"
           src="https://www.youtube.com/embed/HpxYfVUaTH8"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
@@ -35,7 +54,7 @@ export default function Home() {
           className="w-full h-56 mr-5"
           src="https://www.youtube.com/embed/eTHsMtcpg_Q"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
@@ -53,7 +72,7 @@ export default function Home() {
           className="w-full h-56 mr-5"
           src="https://www.youtube.com/embed/tLfNUD0-8ts"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
